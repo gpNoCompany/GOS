3be5ea5c0e0c670dad2192182f815aa996225547
Nikita K <rabot422@gmail.com>
2024-03-05 14:41:39 +0300

    https://tree.taiga.io/project/tuberkul-gos/us/12?kanban-include_attachments=1&kanban-include_tasks=1&kanban-status=9222423 finish styling

--- src/pages/sign-in/ui/index.tsx
+++ src/pages/sign-in/ui/index.tsx
@@ -3,26 +3,26 @@ import FACEBOOK_ICON from '../../../assets/icons/Frame_facebook.png'
 
 export const SignInForm = () => {
   return (
-    <div className="flex justify-center h-screen items-center bg-light-black">
+    <div className="flex justify-around flex-col  h-screen items-center  bg-light-black">
       <form className="w-80">
         <fieldset className="flex flex-col gap-5 text-white mb-5">
           <legend className="text-center text-2xl font-black mb-5">
             Log in to [name]
           </legend>
           <input
-            className="py-3.5 px-3 rounded-lg h-11 bg-dark-gray"
+            className="transition-all hover:bg-input-hover outline-2 outline-input-border-gray py-3.5 px-3 rounded-lg h-11 bg-dark-gray"
             placeholder="Email"
             type="email"
           />
           <input
-            className="py-3.5 px-3 rounded-lg h-11 bg-dark-gray"
+            className="transition-all hover:bg-input-hover outline-2 outline-input-border-gray py-3.5 px-3 rounded-lg h-11 bg-dark-gray"
             placeholder="Password"
             type="password"
           />
-          <button className="text-light-gray text-right">
+          <a href="#" className="text-light-gray text-right">
             Forgot your password?
-          </button>
-          <button className="bg-white text-black h-11 rounded-lg text-xs font-semibold">
+          </a>
+          <button className="transition-all hover:opacity-80 bg-btn-bg-green text-black h-11 rounded-lg text-xs font-semibold">
             Log in
           </button>
         </fieldset>
@@ -30,16 +30,20 @@ export const SignInForm = () => {
           <div className="text-light-gray mx-5">or continue with</div>
         </div>
         <div className="flex flex-col gap-5">
-          <button className="flex gap-3 justify-center items-center bg-btn-bg-black border border-btn-border-black rounded-lg h-11">
+          <button className="transition-all hover:bg-dark-gray flex gap-3 justify-center items-center bg-btn-bg-black border border-btn-border-black rounded-lg h-11">
             <img src={GOOGLE_ICON} alt="Google" />
             <span className="font-bold text-white">Log in</span>
           </button>
-          <button className="flex gap-3 justify-center items-center bg-btn-bg-black border border-btn-border-black rounded-lg h-11">
+          <button className="transition-all hover:bg-dark-gray flex gap-3 justify-center items-center bg-btn-bg-black border border-btn-border-black rounded-lg h-11">
             <img src={FACEBOOK_ICON} alt="Facebook" />
             <span className="font-bold text-white">Log in</span>
           </button>
         </div>
       </form>
+      <a href="#" className="text-light-gray flex gap-1 justify-center ">
+        Don't have an account?
+        <p className="text-white font-bold ">Sign up</p>
+      </a>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors:{
+      colors: {
         'light-black': '#1D1D1D',
         'light-gray': '#5F5F5F',
         'dark-gray': '#2B2B2B',
-        'btn-bg-black':'#222222',
-        'btn-border-black':'#343434',
-      }
+        'btn-bg-green': '#60CA86',
+        'btn-bg-black': '#222222',
+        'btn-border-black': '#343434',
+        'input-border-gray': 'rgba(177, 150, 255, 0.5)',
+        'input-hover': '#383838',
+      },
     },
   },
   plugins: [],
